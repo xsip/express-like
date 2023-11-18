44a821bd6ff8933a2f8ffca781eefc3baea6a2f1
Kerschbaumer Stefan <xsip@pm.me>
2023-11-18 13:00:07 +0100

    adds override functionality

--- src/core/injector.ts
+++ src/core/injector.ts
@@ -3,19 +3,20 @@ import http from 'http';
 
 export class Injector {
   private static depInstances: Record<string, any> = {};
-  static get<T>(target: string | (new (...args: unknown[]) => T) ): T {
-    if(typeof target === 'string') {
+
+  static get<T>(target: string | (new (...args: unknown[]) => T)): T {
+    if (typeof target === 'string') {
       return this.depInstances[target];
     }
 
     const dependencies = Reflect.getMetadata("design:paramtypes", target) || [];
-    const tokenArgs: {index: number; token: string;}[] = Reflect.getMetadata('tokenArgs',target);
-    const instances = dependencies.map((dep: (new (...args: unknown[])=> unknown) | string, idx: number) => {
+    const tokenArgs: { index: number; token: string; }[] = Reflect.getMetadata('tokenArgs', target);
+    const instances = dependencies.map((dep: (new (...args: unknown[]) => unknown) | string, idx: number) => {
       const token = tokenArgs.find(t => t.index === idx);
-      if(token) {
-        if(token.token === 'request')
+      if (token) {
+        if (token.token === 'request')
           return Injector.getRequest();
-        if(token.token === 'response')
+        if (token.token === 'response')
           return Injector.getRequest();
 
         return Injector.get(token.token)
@@ -23,25 +24,30 @@ export class Injector {
       return Injector.get(dep)
     });
 
-    if(Reflect.getMetadata('created', target))
+    if (Reflect.getMetadata('created', target))
       return Injector.depInstances[target.prototype.name];
 
     const instance = new target(...instances);
-    Reflect.defineMetadata('created',true, target);
+    Reflect.defineMetadata('created', true, target);
     Injector.depInstances[target.prototype.name] = instance as any;
     return instance as T;
   }
 
-  static registerToken<T>(token: string, value: T) {
-     this.depInstances[token] = value;
+  static registerToken<T>(token: string, value: T, override: boolean = true) {
+    if (this.depInstances[token] && !override)
+      return;
+    this.depInstances[token] = value;
   }
+
   static getRequest(): http.IncomingMessage {
     return Injector.get<http.IncomingMessage>('request');
   }
+
   static getBody<T>(): T {
     return Injector.get<T>('body');
   }
-  static getResponse(): http.ServerResponse<http.IncomingMessage> & {req: http.IncomingMessage} {
-    return Injector.get<http.ServerResponse<http.IncomingMessage> & {req: http.IncomingMessage}>('response');
+
+  static getResponse(): http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage } {
+    return Injector.get<http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage }>('response');
   }
 }
